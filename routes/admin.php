16f9596b2947a00742c583b61e09e9264b92762b
ammar <amarya211fgff@gmail.com>
2025-04-20 00:33:25 +0200

    Admin login verify and form for login

--- routes/admin.php
+++ routes/admin.php
@@ -11,5 +11,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace'=>'Dashboard', 'middleware'=>'auth:admin'], function () {
+    Route::get('/' , function (){
+        return 'admin';
+    });
+    Route::get('/dashboard' , function (){
+        return 'dashboard';
+    })->name('admin.dashboard');
+});
 
+Route::group(['namespace'=>'Dashboard'], function () {
+   Route::get('/login' ,'LoginController@login') ->name('admin.login');
+   Route::post('/verifyLogin' ,'LoginController@verifyAdmin') ->name('admin.verification');
 });
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', function () {
-    return view('layouts.admin');
+Route::get('login', function () {
+    return 'login web';
 });
